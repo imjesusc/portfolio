c55c272409e36da7f6874c9e88c8b08d5a8e3ed7
imjesusc <iamjesus.ds@gmail.com>
2023-12-24 01:02:45 -0500

    fix: img url not found again

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,6 +6,7 @@ export const Projects = (props) => {
   return (
     <section className="main__projects">
       <h2 className="main__projects--title">Mis proyectos</h2>
+      {JSON.stringify(projects)}
       <div className="main__projects--list">
         {projects && projects.map((projects) => <ProjectCard key={projects.id} project={projects} />)}
       </div>
