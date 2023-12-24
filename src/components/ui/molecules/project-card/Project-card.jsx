db764341a3b69a77d1784005322022050300264b
imjesusc <iamjesus.ds@gmail.com>
2023-12-24 03:39:48 -0500

    feat: Integrated modal to project card

--- src/components/ui/molecules/project-card/Project-card.jsx
+++ src/components/ui/molecules/project-card/Project-card.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react'
+import { Modal } from '../modal/Modal'
 import './Project-card.css'
 export const ProjectCard = (props) => {
   const { project } = props
+  const [showModal, setShowModal] = useState(false)
   return (
     <article className="project">
       <figure className="project__card">
-        <img className="project__card--img" src={project.img} alt={project.name} />
+        <img onClick={() => setShowModal(true)} className="project__card--img" src={project.img} alt={project.name} />
         <figcaption className="project__card--title">{project.name}</figcaption>
       </figure>
 
-      <div className="modal"></div>
+      <Modal project={project} showModal={showModal} setShowModal={setShowModal} />
     </article>
   )
 }
