be206d2079a82b6bc070481479a825fa3aea598b
imjesusc <iamjesus.ds@gmail.com>
2023-12-24 15:26:18 -0500

    refactor: Slider component and added animation

--- src/components/ui/molecules/slider/Slider.jsx
+++ src/components/ui/molecules/slider/Slider.jsx
@@ -1,9 +1,37 @@
-import { SliderCard } from '../../atoms/slider-card/Slider-card'
 import './Slider.css'
+import { useEffect, useRef } from 'react'
+import { SliderCard } from '../../atoms/slider-card/Slider-card'
+
 export const Slider = ({ crafts }) => {
+  const sliderContainer = useRef(null)
+  const slider = useRef(null)
+
+  useEffect(() => {
+    const initAnimation = () => {
+      sliderContainer.current.setAttribute('data-animated', true)
+
+      const originalSliderItems = Array.from(slider.current.children)
+
+      originalSliderItems.forEach((item) => {
+        const duplicateItem = item.cloneNode(true)
+        duplicateItem.setAttribute('aria-hidden', true)
+        slider.current.appendChild(duplicateItem)
+      })
+    }
+
+    initAnimation()
+  }, [])
+
   return (
-    <div className="slider-container">
-      {crafts && crafts.map((craft) => <SliderCard key={craft.id} project={craft} className="slider-item" />)}
+    <div className="slider-container" ref={sliderContainer} data-direction="left" data-speed="slow">
+      <ul className="slider tags-list" ref={slider}>
+        {crafts &&
+          crafts.map((craft) => (
+            <li key={craft.id} className="slider-item" title={craft.name}>
+              <SliderCard project={craft} />
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
