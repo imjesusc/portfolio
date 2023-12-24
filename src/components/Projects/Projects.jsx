0d04f58bf28342f2eac1ee56caeef1649618985f
imjesusc <iamjesus.ds@gmail.com>
2023-12-24 00:53:56 -0500

    feat: Added Projects component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,14 @@
+import { ProjectCard } from '../ui/molecules/Project-card'
+import './Projects.css'
+
+export const Projects = (props) => {
+  const { projects } = props
+  return (
+    <section className="main__projects">
+      <h2 className="main__projects--title">Mis proyectos</h2>
+      <div className="main__projects--list">
+        {projects && projects.map((projects) => <ProjectCard key={projects.id} project={projects} />)}
+      </div>
+    </section>
+  )
+}
