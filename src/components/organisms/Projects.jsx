116cc2cd3d658f111dc50089a1e33c001eb35a43
imjesusc <iamjesus.ds@gmail.com>
2024-01-03 20:32:27 -0500

    feat(Root): Added a new card and update projects component with tailwind

---
+++ src/components/organisms/Projects.jsx
@@ -0,0 +1,28 @@
+import { Card, Title } from '../atoms'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+export const Projects = ({ projects }) => {
+  return (
+    <section className="p-4 grid gap-4 tablet:max-w-[--max-w-screen] m-auto">
+      <Title type="secondary" title="Proyectos" className="font-semibold" />
+
+      <div className={classNames('grid-cols-1', 'm-auto grid', 'tablet:grid-cols-2 gap-4', 'laptop:grid-cols-3')}>
+        {projects.map((project) => (
+          <Card
+            key={project.id}
+            img={project.img}
+            title={project.title}
+            description={project.description}
+            demo={project.demo}
+            repository={project.repository}
+            tecnologies={project.tecnologies}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+Projects.propTypes = {
+  projects: PropTypes.array,
+}
