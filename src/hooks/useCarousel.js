2825aefe3cea1da6816f745784c68a7ae8c4303c
imjesusc <iamjesus.ds@gmail.com>
2024-01-03 23:20:43 -0500

    fix: Change useCaruosel duplicate feature and added validation

--- src/hooks/useCarousel.js
+++ src/hooks/useCarousel.js
@@ -63,13 +63,16 @@ export const useCarousel = (wrapperRef = null, carruselRef = null, cardRef = nul
 
     const originalItems = Array.from(carruselRef.current.children)
 
+    if (originalItems.length <= 3) {
+      originalItems.forEach((item) => {
+        const duplicateItem = item.cloneNode(true)
+        duplicateItem.setAttribute('aria-hidden', true)
+        carruselRef.current.appendChild(duplicateItem)
+      })
+    }
+
     originalItems.forEach((item) => {
       const duplicateItem = item.cloneNode(true)
-      if (originalItems.length <= 3) {
-        const secondaryDuplicateItem = item.cloneNode(true)
-        secondaryDuplicateItem.classList.add('secondary-duplicate-item')
-        duplicateItem.appendChild(secondaryDuplicateItem)
-      }
       duplicateItem.setAttribute('aria-hidden', true)
       carruselRef.current.appendChild(duplicateItem)
     })
